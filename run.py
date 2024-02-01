b56e76e3564d0136ad813c063939709798dd8896
roman <romanbokhyan@gmail.com>
2024-02-01 19:25:56 +0300

    added config for running the EF

--- run.py
+++ run.py
@@ -3,5 +3,12 @@ from deap import base, creator, tools, algorithms
 from event_frames.event_frame_generator import EventFrameManager
 from event_filter.event_filter_generator import EventFilter
 
-EF = EventFilter("raw_data/high_speed_cam_videos/MotorAmplif_motion_evm_2022-12-23-10-50-19.mp4")
 
+EF_manager = EventFrameManager("raw_data/high_speed_cam_videos/MotorAmplif_motion_evm_2022-12-23-10-50-19.mp4")
+EF_manager.extract_frames()
+EF_manager.convert_to_grayscale()
+EF_manager.create_event_frames()
+
+
+EF = EventFilter(EF_manager)
+EF.iteration()
